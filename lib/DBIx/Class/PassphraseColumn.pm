2a2bca67a747d68e173c7a5fadff6b3e315e3f26
Karen Etheridge <ether@cpan.org>
2019-06-06 18:04:37 -0700

    increment $VERSION after 0.03 release

--- lib/DBIx/Class/PassphraseColumn.pm
+++ lib/DBIx/Class/PassphraseColumn.pm
@@ -4,7 +4,7 @@ use warnings;
 package DBIx::Class::PassphraseColumn;
 # ABSTRACT: Automatically hash password/passphrase columns
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 use Class::Load 'load_class';
 use Sub::Name 'subname';
